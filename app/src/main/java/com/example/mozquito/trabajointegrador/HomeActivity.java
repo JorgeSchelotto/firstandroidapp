f55bf4b68a6c361c76889777c26aec539c6c7b46
Schelotto, Jorge Alberto <jasfotografo@hotmail.com>
2018-08-20 23:57:09 -0300

    Clase 1

--- app/src/main/java/com/example/mozquito/trabajointegrador/HomeActivity.java
+++ app/src/main/java/com/example/mozquito/trabajointegrador/HomeActivity.java
@@ -2,16 +2,12 @@ package com.example.mozquito.trabajointegrador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    private EditText etUsuario;
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        etUsuario = findViewById(R.id.etUsuario);
+        setContentView(R.layout.activity_home);
     }
 }
