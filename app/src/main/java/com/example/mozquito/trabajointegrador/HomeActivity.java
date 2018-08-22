fb41df66192329439873fd60fa9247e4c9382b67
Schelotto, Jorge Alberto <jasfotografo@hotmail.com>
2018-08-21 22:57:51 -0300

    Clase 3 Parte A

--- app/src/main/java/com/example/mozquito/trabajointegrador/HomeActivity.java
+++ app/src/main/java/com/example/mozquito/trabajointegrador/HomeActivity.java
@@ -2,12 +2,26 @@ package com.example.mozquito.trabajointegrador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private String usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        assignIntentVariable();
+        Toast.makeText(this, "Hola " + usuario, Toast.LENGTH_SHORT).show();
+    }
+
+    private void assignIntentVariable() {
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            usuario = bundle.getString("usuario");
+        }
     }
 }
--- app/src/main/java/com/example/mozquito/trabajointegrador/MainActivity.java
+++ app/src/main/java/com/example/mozquito/trabajointegrador/MainActivity.java
@@ -1,17 +1,70 @@
 package com.example.mozquito.trabajointegrador;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etUsuario;
+    private EditText etContraseña;
+    private Button btnIniciarsesion;
+    private Button btnCrearUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUi();
+
+    }
+
+    private void setupUi() {
         etUsuario = findViewById(R.id.etUsuario);
+        etContraseña = findViewById(R.id.etContraseña);
+        btnIniciarsesion = findViewById(R.id.btnIniciarSesion);
+        btnCrearUsuario = findViewById(R.id.btnCrearUsuario);
+
+        btnIniciarsesion.setOnClickListener(this);
+        btnCrearUsuario.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId();
+
+        if (id == R.id.btnIniciarSesion) {
+            iniciarSesion();
+        } else if (id == R.id.btnCrearUsuario) {
+            Toast.makeText(this, "Cear Usuario", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    private void iniciarSesion() {
+
+        String usuario = etUsuario.getText().toString();
+        String contraseña = etContraseña.getText().toString();
+
+        if (usuario.isEmpty() || contraseña.isEmpty()) {
+            Toast.makeText(this, "Completar los campos", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+
+            intent.putExtra("usuario", usuario);
+            intent.putExtra("contraseña", contraseña);
+
+            startActivity(intent);
+
+
+        }
     }
 }
